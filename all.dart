69df79c439618cde88afbd96f6856ea385a37d09
Robert Mollentze <robmllze@gmail.com>
2023-04-30 03:37:55 -0400

    update

--- all.dart
+++ all.dart
@@ -14,6 +14,3 @@ export 'src/services/all_services.dart';
 export 'src/themes/all_themes.dart';
 export 'src/utils/all_utils.dart';
 export 'src/widgets/all_widgets.dart';
-
-// Copy `xyz_shared/COPY_ME_TO_LIB_FOLDER.xyz_config.dart` to `lib/xyz_config.dart`.
-export '../xyz_shared_config.dart';
