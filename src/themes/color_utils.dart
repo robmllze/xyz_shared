63dd56c47ee0583bc526e17bed378f194c3b9d22
Robert Mollentze <robmllze@gmail.com>
2023-04-30 00:50:04 -0400

    update

--- src/themes/color_utils.dart
+++ src/themes/color_utils.dart
@@ -2,7 +2,7 @@ import 'package:flutter/painting.dart';
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-extension ColorInverted on Color {
+extension ColorUtils on Color {
   Color get inverted {
     return Color.fromRGBO(
       255 - this.red,
@@ -10,5 +10,28 @@ extension ColorInverted on Color {
       255 - this.blue,
       this.opacity,
     );
+  }
+
+  Color withShadeCode(int shadeCode) {
+    final hsl = HSLColor.fromColor(this);
+    final lighter = hsl.withLightness((hsl.lightness + shadeCode / 900) % 1);
+    return lighter.toColor();
+  }
+
+  Color withSaturation(double saturation) {
+    final hsl = HSLColor.fromColor(this);
+    return hsl.withSaturation(saturation).toColor();
+  }
+
+  Color lighten(Color color, double amount) {
+    final hsl = HSLColor.fromColor(color);
+    final darkened = hsl.withLightness(hsl.lightness * amount);
+    return darkened.toColor();
+  }
+
+  Color darken(Color color, double amount) {
+    final hsl = HSLColor.fromColor(color);
+    final darkened = hsl.withLightness(hsl.lightness * (1 - amount));
+    return darkened.toColor();
   }
 }
